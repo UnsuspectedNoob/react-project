eee24a12f24560f69d4b2f27448c38a33fd41ed0
Dark Emrys <mathersmarshall948@gmail.com>
2023-06-26 11:51:17 +0100

    Add styles in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
+    <div className='text-center text-8xl text-red-500 uppercase'>
       This is the start of something new.
     </div>
   );
