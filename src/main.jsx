b45b917a4aa36da01fb0a0a1c61bf49635325b01
Dark Emrys <mathersmarshall948@gmail.com>
2023-07-01 11:56:36 +0100

    added createBrowserRouter instead of BrowserRouter and used RouterProvider added layouts folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+
+import {
+
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+
+} from 'react-router-dom'
+
 import './index.css'
+import Root from './layouts/Root.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={ <Root /> } >
+      <Route index element={ <Home /> } />
+      <Route path='about' element={ <About /> } />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={ router } />
   </React.StrictMode>,
 );
